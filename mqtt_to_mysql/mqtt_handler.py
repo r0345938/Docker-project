c9424c5a16db8ff1631a9898feccaa13148a8668
r0345938 <r0345938@student.thomsamore.be>
2024-11-14 14:14:03 +0100

    First commit on master branch

--- mqtt_to_mysql/mqtt_handler.py
+++ mqtt_to_mysql/mqtt_handler.py
@@ -8,12 +8,12 @@ logger = logging.getLogger(__name__)
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         client.subscribe("sensor/data")
-        logger.info(f"Verbonden met MQTT-broker en geabonneerd op 'sensor/data'")
+        logger.info(f"Connected to MQTT broker and subscribed to 'sensor/data'")
     else:
-        logger.error(f"Fout bij verbinden met MQTT-broker. Return code: {rc}")
+        logger.error(f"Error connecting with MQTT-broker. Return code: {rc}")
 
 def on_message(client, userdata, msg):
-    logger.info(f"Bericht ontvangen op topic {msg.topic}: {msg.payload.decode()}")
+    logger.info(f"Message received on topic {msg.topic}: {msg.payload.decode()}")
 
 def setup_mqtt_client():
     client = Client()
@@ -24,5 +24,5 @@ def setup_mqtt_client():
         client.connect("mqtt-broker", 1883, 60)
         return client
     except Exception as e:
-        logger.error(f"Verbinding maken met MQTT-broker mislukt: {e}")
+        logger.error(f"Connection faillure with MQTT-broker: {e}")
         raise e
