9fe990b16f92ae2307540486b756bf0f8544bc21
r0345938 <r0345938@student.thomsamore.be>
2024-11-27 16:20:52 +0100

    Updating test file

--- tests/test_data_units.py
+++ tests/test_data_units.py
@@ -13,16 +13,31 @@ class TestDataUnits(unittest.TestCase):
         )
         self.cursor = self.db_connection.cursor()
 
+        # Insert testdata
+        insert_query = "INSERT INTO sensor_data (temperature, pressure, timestamp) VALUES (%s, %s, NOW())"
+        test_data = [
+            (20.0, 1020.0),  
+            (25.0, 1000.0), 
+            (15.0, 999.0) # Test faillure
+        ]
+        self.cursor.executemany(insert_query, test_data)
+        self.db_connection.commit()  # Commit data to database
+
     def test_units(self):
+        # excecute query to collect data
         self.cursor.execute("SELECT temperature, pressure FROM sensor_data")
-        for (temperature, pressure) in self.cursor:
-            self.assertGreaterEqual(temperature, 20, "Temperature cannot be lower or equal as 20° Celsius.")
-            self.assertGreaterEqual(pressure, 1000, "Pressure cannot be lower or equal as 1000 in hPa.")
+        result = self.cursor.fetchall()  # Collecting values
+        print(result)  # Debug to check the value of the data
+
+        # Assertion to check values are correct
+        for (temperature, pressure) in result:
+            self.assertGreaterEqual(temperature, 20, "Temperature cannot be lower than 20° Celsius.")
+            self.assertGreaterEqual(pressure, 1000, "Pressure cannot be lower than 1000 hPa.")
 
     def tearDown(self):
+        # close cursor and connection
         self.cursor.close()
         self.db_connection.close()
 
 if __name__ == '__main__':
     unittest.main()
-
